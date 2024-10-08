2aec6da067fa0ed64ca88c5f69122cc8c03fc51d
darknoise.francesco <darknoise.francesco@gmail.com>
2024-10-08 09:29:21 +0000

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:683339850.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2698507315.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1959221753.
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <h1>Login</h1>
+  <form method="post" action="login.php">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username"><br><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
